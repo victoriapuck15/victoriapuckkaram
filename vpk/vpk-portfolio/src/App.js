317fcbea76cd98acfaaacd22f1d826ac5c9d371c
victoriapuck15 <victoriapuck15@gmail.com>
2025-07-28 01:00:56 -0400

    current changes

--- vpk/vpk-portfolio/src/App.js
+++ vpk/vpk-portfolio/src/App.js
@@ -2,8 +2,13 @@ import React, { useEffect, useState, useRef } from "react";
 import "./App.css";
 import toya from "./toya.JPG";
 import { BiLogoGithub } from "react-icons/bi";
-import { BiMailSend } from "react-icons/bi";
+import { MdOutlineMailOutline } from "react-icons/md";
 import { BiLogoLinkedinSquare } from "react-icons/bi";
+//import { DiPython,DiNodejsSmall,DiJavascript,DiJava,DiPostgresql,DiMongodb,DiSqllite} from "react-icons/di";
+//import { SiTerraform } from "react-icons/si";
+import Flicking, { useFlickingReactiveAPI } from "@egjs/react-flicking";
+import Panel from "@site/src/component/Panel";
+
 
 function App() {
   const [navScrolled, setNavScrolled] = useState(false);
@@ -36,58 +41,12 @@ function App() {
       <HeroSection />
 
       <main className="content">
-  
-
-        <Section title="Projects" id="projects">
-          <div className="card-grid">
-            <ProjectCard
-              title="CMCI Index"
-              description="A quantative approach to consumer sentiment"
-              color="#5B2E48"
-              link="#"
-            />
-            <ProjectCard
-              title="Demand Execution Dashboard"
-              description="MAXAR Intelligence Supply & Demand Analysis Project."
-              color="#76A9C4"
-              link="#"
-            />
-            <ProjectCard
-              title="Spot Suggest"
-              description="Machine Learning tool to make song recommendations."
-              color="#4C6663"
-              link="#"
-            />
-            <ProjectCard
-              title="X-READ"
-              description="An AI-powered chatbot for customer support."
-              color="#A6B07E"
-              link="#"
-            />
-            <ProjectCard
-              title="Digital Healthcare Accessbility Project"
-              description="Leveraging webscraping to perform accessibility analysis."
-              color="#B86F52"
-              link="#"
-            />
-            <ProjectCard
-              title="Course Work"
-              description="Studies and Course Work at Penn State."
-              color="#9FA4C4"
-              link="#"
-            />
-          </div>
-        </Section>
+        <ProjectsSlider/>
+       
         <Section title="Skills" id="skills">
           <SkillsGrid />
         </Section>
 
-        <Section title="Contact" id="contact">
-          <p>
-            Let’s connect! Reach me at{" "}
-            <a href="mailto:youremail@example.com">youremail@example.com</a>.
-          </p>
-        </Section>
       </main>
     </div>
   );
@@ -98,10 +57,11 @@ function Navigation({ scrolled }) {
     <nav className={`navigation ${scrolled ? "scrolled" : ""}`}>
       <a href="#about">About</a>
       <a href="#projects">Projects</a>
-      <a href="#contact">Contact</a>
+      <a href="#contact">Resume</a>
     </nav>
   );
 }
+
 
 function HeroSection() {
   return (
@@ -131,59 +91,28 @@ function Section({ title, children, id }) {
   );
 }
 
-function ProjectCard({ title, description, color, link }) {
-  const style = { backgroundColor: color };
-  const cardRef = useRef(null);
-
-  const [transformStyle, setTransformStyle] = useState({});
-
-  const handleMouseMove = (e) => {
-    const card = cardRef.current;
-    const rect = card.getBoundingClientRect();
-
-    const x = e.clientX - rect.left;
-    const y = e.clientY - rect.top;
-
-    const centerX = rect.width / 2;
-    const centerY = rect.height / 2;
-
-    const rotateX = ((y - centerY) / centerY) * 15;
-    const rotateY = ((centerX - x) / centerX) * 15;
-
-    setTransformStyle({
-      transform: `perspective(600px) rotateX(${rotateX}deg) rotateY(${rotateY}deg) scale(1.05)`,
-      transition: "transform 0.1s",
-    });
-  };
-
-  const handleMouseLeave = () => {
-    setTransformStyle({
-      transform: "perspective(600px) rotateX(0deg) rotateY(0deg) scale(1)",
-      transition: "transform 0.5s ease",
-    });
-  };
-
+function ProjectsSlider(){
+  const flickingRef = React.createRef();
+  const {
+    indexProgress
+  } = useFlickingReactiveAPI(flickingRef);
+  const length = 5;
   return (
-    <div
-      className="card fade-section"
-      style={{ ...style, ...transformStyle }}
-      ref={cardRef}
-      onClick={() => (window.location.href = link)}
-      onMouseMove={handleMouseMove}
-      onMouseLeave={handleMouseLeave}
-      role="button"
-      tabIndex={0}
-      onKeyPress={(e) => {
-        if (e.key === "Enter") window.location.href = link;
-      }}
-    >
-      <div className="card-text">
-        <h3>{title}</h3>
-        <p>{description}</p>
-      </div>
-    </div>
+    <Flicking ref={flickingRef} circular={true} className="flicking-coverflow">
+      {[0, 1, 2, 3, 4].map((index) => {
+        const childProgress = (index - indexProgress + length * 1.5) % length -  length * 0.5;
+        const scale = Math.max(0, 0.9 - Math.abs(childProgress) * 0.2);
+
+        return <Panel key={index} index={index} style={{
+          transformOrigin: `${50 - childProgress * 50}% 50%`,
+          transform: `rotateY(${-childProgress * 50}deg) scale(${scale})`
+        }} />;
+      })}
+    </Flicking>
   );
 }
+
+
 
 function SkillsGrid() {
   const skills = [
@@ -211,7 +140,7 @@ function SocialIcons() {
     <div className="social-icons">
     
       <a href="mailto:vbp5103@gmail.com" target="_blank" rel="noopener noreferrer">
-      <BiMailSend size={35} />
+      <MdOutlineMailOutline size={35} />
       </a>
       <a href="https://www.linkedin.com/in/victoriapuckkaram/" target="_blank" rel="noopener noreferrer">
         <BiLogoLinkedinSquare size={35} />
