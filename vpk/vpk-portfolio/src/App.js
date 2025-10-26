1c6a3cd58b983ea3e5369c80e1558e1866741bcd
victoriapuck15 <victoriapuck15@gmail.com>
2025-10-26 18:52:31 -0400

    updated resume link

--- vpk/vpk-portfolio/src/App.js
+++ vpk/vpk-portfolio/src/App.js
@@ -131,7 +131,7 @@ function HeroSection() {
           Passionate about building scalable, data-driven systems, specializing in backend engineering and cloud infrastructure.
         </p>
         <a
-          href="vpk-portfolio/public/Victoria_Puck_Karam_Resume_2025.pdf" 
+          href="https://drive.google.com/file/d/1ZG992MIikTd69tzHYWYzLOoYjl1D87AH/view?usp=sharing" 
           download
           className="resume-button"
         >
