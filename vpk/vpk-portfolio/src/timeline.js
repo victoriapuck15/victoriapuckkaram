4e66a5e8469e1f3c0e3f35f874d1033ffa0802cb
victoriapuck15 <victoriapuck15@gmail.com>
2026-02-24 20:47:42 -0500

    added powershell

--- vpk/vpk-portfolio/src/timeline.js
+++ vpk/vpk-portfolio/src/timeline.js
@@ -5,7 +5,7 @@ const timelineData = [
     company: "JPMorgan Chase & Co.",
     role: "Software Engineer II",
     description: "Top 5% global Software Engineer II driving AI-powered platform modernization, transforming monoliths into scalable microservices, and delivering LLM solutions that scaled to thousands of users.",
-    skills: ["devicon-spring-original","devicon-amazonwebservices-plain-wordmark","devicon-terraform-plain","devicon-jenkins-plain","devicon-python-plain","devicon-docker-plain","devicon-maven-plain","devicon-react-original", "devicon-dynatrace-plain","devicon-cucumber-plain","devicon-javascript-plain", "devicon-splunk-original-wordmark","devicon-bash-plain" ],
+    skills: ["devicon-spring-original","devicon-amazonwebservices-plain-wordmark","devicon-terraform-plain","devicon-jenkins-plain","devicon-python-plain","devicon-docker-plain","devicon-maven-plain","devicon-react-original", "devicon-dynatrace-plain","devicon-cucumber-plain","devicon-powershell-plain","devicon-javascript-plain", "devicon-splunk-original-wordmark","devicon-bash-plain" ],
     details: {
     bullets: [
       "Recognized in the top 5% of global early-career software engineers and fast-tracked for promotion based on performance and impact.",
